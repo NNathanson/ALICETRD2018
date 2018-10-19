c26a75382d2cfaacee2ef72785f1c32d47cbfc0b
WianZeeman <ZMNWIL001@myuct.ac.za>
2018-10-19 16:33:17 +0200

    Rewrite tbsum.py to actually produce a histogram
    Up to now, tbsum.py only summed the events and produced a bar chart.
    Now, it produces a histogram where you can see the frequency of
    different ADC values.

--- tbsum.py
+++ tbsum.py
@@ -1,49 +1,41 @@
 """python3 tbsum.py [FILE...]
 
-Produce a histogram of interesting events summed over time bins.
+Produce a histogram of events summed over time bins.
 
-Extract interesting events from raw data files, sum the data arrays, sum
-over the time bins, and save a histogram in a file called tbsum.png.
+Extract events from raw data files, sum over the time bins, and save a
+histogram in a file called tbsum.png.
 
 Parameters
 ----------
-FILE : list of arbitrary length of paths to raw data files, relative to
-this file's directory.  If this parameter is absent, process all files in `defaults.DEFAULT_DATA_FOLDER`.
+FILE : str
+    list of arbitrary length of paths to raw data files, relative to this
+    file's directory.  If this parameter is absent, process all files in
+    `defaults.DEFAULT_DATA_FOLDER`.
 """
 import defaults
 import o32reader as rdr
 import adcarray as adc
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import glob
 import sys
 
 array_shape = (12, 144, 30)
+num = 2000
+bins = np.linspace(1, 20000, num)
+
 def sum_events(path):
-    """Extract interesting events from a file, and sum the data arrays.
+    """Extract events from a file, and sum the data arrays.
     
     Parameters
     ----------
     path : str
         Path to a raw data file
-
-    Returns
-    -------
-    data_sum : array of shape (12, 144, 30)
-        The sum of all interesting events, without summing over time
     """
     reader = rdr.o32reader(path)
     analyser = adc.adcarray()
+    inner_hist, bin_edges = np.histogram((), bins)
 
-    def is_interesting(data):
-        return np.max(data) > defaults.DEFAULT_BASELINE
-
-    maxevno = None
-    minevno = None
-    absmax = None
-    absmin = None
-    data_sum = np.zeros(array_shape)
     for evno, raw_data in enumerate(reader):
         if evno % defaults.PRINT_EVNO_EVERY == 0:
             print("Proccessing events %d–%d"
@@ -60,34 +52,21 @@ def sum_events(path):
             continue
         data = analyser.data[:12] # The last four rows are zeros.
         data[defaults.DATA_EXCLUDE_MASK] = 0.0
+        tbsum = np.sum(data, 2)
 
-        maxval = np.max(data)
-        minval = np.min(data)
-
-        if absmax is None or maxval > absmax:
-            absmax = maxval
-            maxevno = evno
-
-        if absmin is None or minval < absmin:
-            absmin = minval
-            minevno = evno
-
-        if is_interesting(data):
-            data_sum += data
-    return data_sum
+        foo, bar = np.histogram(tbsum.flatten(), bins)
+        inner_hist += foo
+    return inner_hist
 
 if __name__ == '__main__':
-    fig =  plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    tbsum = np.zeros(array_shape[:-1])
-    x = np.arange(tbsum.shape[0])
-    y = np.arange(tbsum.shape[1])
-    xx, yy = np.meshgrid(x, y, indexing='ij')
     if len(sys.argv) <= 1:
         files = glob.glob(defaults.DEFAULT_DATA_FOLDER + '/*')
     else:
         files = sys.argv[1:]
+    hist, bin_edges = np.histogram((), bins)
     for f in files:
-        tbsum += np.sum(sum_events(f), 2)
-    ax.bar3d(xx.ravel(), yy.ravel(), 0, 1, 1, tbsum.ravel())
-    fig.savefig('tbsum')
+        hist += sum_events(f)
+    print('The maximum in hist is %g.' % (np.amax(hist)))
+    plt.bar(bins[:-1], hist, bins[1] - bins[0], log=True)
+    plt.xlabel('ADC Value')
+    plt.savefig('tbsum.png', bbox_inches='tight')
