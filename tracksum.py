b8c818ca46d007928c842ddba3169b692a13eab2
WianZeeman <ZMNWIL001@myuct.ac.za>
2018-10-22 18:16:59 +0200

    Calculate time bin sum
    tracksum.py now has a for loop for calculating time bin sums from a data
    file.

--- tracksum.py
+++ tracksum.py
@@ -6,3 +6,35 @@ Paramters
 ---------
 file : relative path to a data file
 """
+import sys
+import o32reader as rdr
+import adcarray as adc
+import numpy as np
+
+if len(sys.argv) != 2:
+    print('Incorrect use of tracksum.py.  Type', file=sys.stderr)
+    print('python3 tracksum.py file', file=sys.stderr)
+    print('where file is the path to a data file.', file=sys.stderr)
+    sys.exit()
+reader = rdr.o32reader(sys.argv[1])
+analyser = adc.adcarray()
+bins = np.linspace(1, 14000, 2000)
+hist, bin_edges = np.histogram([], bins)
+for evno, raw_data in enumerate(reader):
+    if evno % defaults.PRINT_EVNO_EVERY == 0:
+        print("Proccessing events %d–%d"
+                % (evno, evno + defaults.PRINT_EVNO_EVERY))
+
+    if evno == 0:
+        # Skip the first event as it may be a configuration event
+        # depending on run configurations.
+        continue 
+
+    try:
+        analyser.analyse_event(raw_data)
+    except adc.datafmt_error as e:
+        continue
+    data = analyser.data[:12] # The last four rows are zeros.
+    data[defaults.DATA_EXCLUDE_MASK] = 0.0
+    tbsum = np.sum(data, 2)
+    flat = np.ravel(tbsum)
